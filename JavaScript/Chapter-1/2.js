8e1d3a8b2ed04ca623d544d544e7d6c3f6643e0c
Hemrajjj13 <iamhemraj13@gmail.com>
2025-11-19 17:03:48 +0530

    Learn about follwing things 1. Numbers, operations, NaN, operator precendence, variables (let, const, var), solved prctice questions

---
+++ JavaScript/Chapter-1/2.js
@@ -0,0 +1,79 @@
+/********************************************/
+// Numbers in Js
+/********************************************/
+
+c = 45;
+typeof c // ----> 'number'
+
+price = 45.99
+typeof price //---> 'number'
+
+negative = -68
+typeof negative //  ----> 'number'
+
+
+/********************************************/
+// Operations in Js 
+/********************************************/
+
+a = 20
+b = 10
+
+//addition
+sum = a + b
+
+//substraction
+diff = a - b
+
+//multiplication 
+product = a * b
+
+//moudulo
+rem = a % b
+
+//division
+div = a / b
+
+// Exponentiation (power operator)
+expo = a ** b
+
+
+/********************************************/
+// NaN in JavaScript
+/********************************************/
+
+0 / 0
+NaN - 1
+NaN * 1
+NaN * NaN
+
+
+/********************************************/
+// Operator Precendence
+/********************************************/
+// JS Follows 
+/* 
+() ---> ** ---> *, /, %  ---> +, -
+*/
+5 + 4 - 4 / 2 * 3 + (2 + 4);
+
+
+/********************************************/
+// variables --> let const var
+/********************************************/
+
+let age = 23;
+age = age + 5;
+
+let b;
+
+let num1 = 5, num2 = 6;
+let finalSum = num1 + num2;
+
+const num = 5;
+num = num * 2; // error
+
+var x = 5, y = 10;
+var z = x + y;
+var z = 25;
+
---
+++ JavaScript/Chapter-1/3.js
@@ -0,0 +1,11 @@
+//Q1. Value of age after this code runs?
+let age = 23;
+age + 2;
+console.log(age);
+
+
+//Q2. Value of avg after this code runs?
+let hindi = 80, eng = 90, math = 100;
+let avg = (hindi + eng + math) / 3;
+console.log(avg);
+
