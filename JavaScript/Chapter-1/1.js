440016d8a23a169f4258ff9d6e5c42186218f5ad
Hemrajjj13 <iamhemraj13@gmail.com>
2025-11-19 14:53:45 +0530

    learn about primitive data types and console

---
+++ JavaScript/Chapter-1/1.js
@@ -0,0 +1,20 @@
+1
+2
+1+2
+1+3
+4-5
+
+
+a = 10;
+typeof a;
+b = 5;
+age = 23;
+name = 'tony';
+console.log(typeof name);
+typeof 'a'
+typeof 55
+
+
+a
+a + b
+// these commands are run inside console
