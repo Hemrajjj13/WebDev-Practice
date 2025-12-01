72a982336d7e2f2045c4838bc2e4767b354fc985
Hemrajjj13 <iamhemraj13@gmail.com>
2025-12-01 16:57:22 +0530

    Learning and applying DOM

--- JavaScript/Chapter-9/app.js
+++ JavaScript/Chapter-9/app.js
@@ -1,24 +1,91 @@
+/*********************  ACCESSING ELEMENTS  **********************/
+
 // getElementById()
-let imgObject = document.getElementById("mainImg");
-console.log(imgObject);
+let img = document.getElementById("mainImg");
+console.log(img);
 
 // getElementsByClassName()
 let smallImages = document.getElementsByClassName("oldImg");
 console.log(smallImages);
 
-for (let i = 0; i < smallImages.length; i++) {
-    smallImages[i].src = "assets/spiderman_img.png";
-    console.log(`value of imge ${i} is changed`);
-}
-
 // getElementsByTagName()
-document.getElementsByTagName("p");
+let allParagraphs = document.getElementsByTagName("p");
 
-//  querySelector() / querySelectorAll()
-
-console.log(document.querySelector("p"));
+// querySelector / querySelectorAll
+let paraG = document.querySelector("p");
 console.log(document.querySelector("#description"));
-console.log(document.querySelector(".box"))
+console.log(document.querySelector(".box"));
 console.log(document.querySelectorAll("div a"));
 
-// 
+
+/*********************  MANIPULATING CONTENT  **********************/
+
+console.log(paraG.textContent);
+console.log(paraG.innerText);
+console.log(paraG.innerHTML);
+
+
+/*********************  MANIPULATING ATTRIBUTES  **********************/
+
+console.log(img.getAttribute("id"));
+// img.setAttribute("src", "assets/creation_2.jpeg");
+
+
+/*********************  MANIPULATING STYLES  **********************/
+
+img.style.border = "3px solid purple";
+
+let links = document.querySelectorAll(".box a");
+for (let link of links) {
+    link.style.color = "crimson";
+}
+
+
+/*********************  CLASSLIST (ADD / REMOVE / TOGGLE)  **********************/
+
+console.log(img.classList);
+
+let heading = document.querySelector("h1");
+heading.classList.add("red");
+
+console.log(heading.classList);
+console.log(heading.classList.contains("red"));
+
+heading.classList.toggle("red");
+
+let box = document.querySelector(".box");
+box.classList.toggle("yellowBg");
+
+
+/*********************  NAVIGATION (PARENT / CHILDREN)  **********************/
+
+let h4 = document.querySelector("h4");
+console.log(h4.parentElement);
+console.log(box.children);
+console.log(box.childElementCount);
+
+
+/*********************  ADDING ELEMENTS  **********************/
+
+let newP = document.createElement("p");
+newP.innerText = "Hi, I am newP.";
+
+box.appendChild(newP);
+box.prepend(newP);
+
+let btn = document.createElement("button");
+btn.innerText = "Click Me";
+
+box.appendChild(btn);
+newP.append(" this is new text");
+
+let firstPara = document.querySelector("p");
+firstPara.insertAdjacentElement("afterbegin", btn);
+
+
+/*********************  REMOVING ELEMENTS  **********************/
+
+let h2 = document.querySelector("h2");
+if (h2) {
+    h2.remove(); // Safely remove if it exists
+}
