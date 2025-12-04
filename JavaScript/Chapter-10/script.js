b20901f5efa1028bb6620a7989737be616795293
Hemrajjj13 <iamhemraj13@gmail.com>
2025-12-04 14:28:28 +0530

    Gathered all Html Codes

---
+++ JavaScript/Chapter-10/script.js
@@ -0,0 +1,15 @@
+let btn = document.querySelector('button');
+console.log(btn);
+
+// btn.onclick = sayHello;
+
+btn.addEventListener('click', sayHello)
+function sayHello() {
+    alert("Hello");
+}
+
+let a = document.querySelector('a');
+
+a.addEventListener('click', function() {
+    console.log("Callback")
+})
