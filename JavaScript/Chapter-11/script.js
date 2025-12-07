cc94154600f80028eccf9f79a0503169cc0dd2a5
Hemrajjj13 <iamhemraj13@gmail.com>
2025-12-07 19:58:08 +0530

    // callback nesting -> callback hell

--- JavaScript/Chapter-11/script.js
+++ JavaScript/Chapter-11/script.js
@@ -14,3 +14,19 @@ console.log("Calling demo function");
 demo();
 console.log("DONE");
 
+// callback hell
+h1 = document.querySelector("h1");
+ 
+function changeColor(color, delay, nextColorChange) {
+    setTimeout(() => {
+        h1.style.color = color;
+        if (nextColorChange) nextColorChange();
+    }, delay);
+}
+
+changeColor("red", 1000, () => {
+    changeColor("orange", 1000, () => {
+        changeColor("yellow", 1000);
+    })
+})
+// callback nesting -> callback hell
