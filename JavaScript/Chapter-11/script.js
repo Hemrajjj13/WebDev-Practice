8e693e3ae1e2317e82ebccc01c3dc08824859ac1
Hemrajjj13 <iamhemraj13@gmail.com>
2025-12-07 19:31:04 +0530

    iplemented call stack

---
+++ JavaScript/Chapter-11/script.js
@@ -0,0 +1,16 @@
+// call stack
+function hello() {
+    console.log("Inside hello function")
+    console.log("hello");
+}
+
+function demo() {
+    console.log("Calling hello function");
+    hello();
+}
+
+
+console.log("Calling demo function");
+demo();
+console.log("DONE");
+
