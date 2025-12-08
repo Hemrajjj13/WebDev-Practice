9f703e344ea1db515854f3e172b215c725800868
Hemrajjj13 <iamhemraj13@gmail.com>
2025-12-08 11:57:51 +0530

    refactor saveToDb to return a Promise instead of using callbacks

---
+++ JavaScript/Chapter-11/promises.js
@@ -0,0 +1,46 @@
+console.log("******** Promises ********");
+// Promises - it represents the eventual completion (or failure of an asynchrous operation)
+
+// function svaToDb(data, success, failure) {
+//     let internetSpeed = Math.floor(Math.random()*10);
+
+//     if (internetSpeed > 4) {
+//         success();
+//     } else {
+//         failure();
+//     }
+
+// }
+
+// saveToDb(
+//     "Hemraj Suryawanshi", 
+//     () => {
+//         console.log("success: your data was saved");
+//         svaToDb("Hello world",
+//             () => {
+//                 console.log("success2: your data2 was saved")
+//             },
+//             () => {
+//                 console.log("failure2: weak connection. data2 not saved");
+//             }
+//         )
+//     }, 
+//     () => {
+//         console.log("failure: weak connection. data not saved");
+//     }
+// );
+
+function saveToDb(data) {
+
+    return new Promise((resolve, reject) => {
+        let internetSpeed = Math.floor(Math.random()*10) + 1;
+
+        if (internetSpeed > 4) {
+            resolve("success: Data saved ");
+        } else {
+            reject("failed: Data not daved ");
+        }
+    });
+}
+
+saveToDb("Hemraj");
