975fdc53cd553be5bd2d88b30ed246b3db66ce61
Hemrajjj13 <iamhemraj13@gmail.com>
2025-12-14 15:04:39 +0530

    Implemented Api Request

--- JavaScript/Chapter-12/script.js
+++ JavaScript/Chapter-12/script.js
@@ -29,3 +29,17 @@ async function loadData() {
     throw err;
   }
 }
+
+
+let url = "https://catfact.ninja/fact";
+
+fetch(url)
+  .then((res)=> {
+    console.log(res);
+    res.json().then((data) => {
+      console.log(data.fact);
+    })
+  })
+  .catch(err => {
+    console.log("Error - ", err);
+  })
