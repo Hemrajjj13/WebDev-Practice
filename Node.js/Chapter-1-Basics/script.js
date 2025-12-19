97f0c1005583415fcec2df69176add959634acd1
Hemrajjj13 <iamhemraj13@gmail.com>
2025-12-19 14:35:19 +0530

    exporting the functions variables into another file

--- Node.js/Chapter-1-Basics/script.js
+++ Node.js/Chapter-1-Basics/script.js
@@ -9,8 +9,12 @@
 
 // console.log(process.argv);
 
-let args = process.argv;
+// let args = process.argv;
 
-for (let i = 2; i < args.length; i++) {
-    console.log("hello to ", args[i]);
-}
+// for (let i = 2; i < args.length; i++) {
+//     console.log("hello to ", args[i]);
+// }
+
+const obj = require("./math.js");
+
+console.log(obj.sum(9,2435));
