4d26ef26f13d4a3b951de665bc9f0623110a07cb
Hemrajjj13 <iamhemraj13@gmail.com>
2025-12-19 14:18:52 +0530

    learn about process.argv

--- Node.js/Chapter-1-Basics/script.js
+++ Node.js/Chapter-1-Basics/script.js
@@ -1,5 +1,16 @@
-let n = 5;
+// let n = 5;
 
-for (let i = 0; i < n; i++) {
-    console.log("hello, ", i);
+// for (let i = 0; i < n; i++) {
+//     console.log("hello, ", i);
+// }
+
+// console.log("Good Job");
+
+
+// console.log(process.argv);
+
+let args = process.argv;
+
+for (let i = 2; i < args.length; i++) {
+    console.log("hello to ", args[i]);
 }
